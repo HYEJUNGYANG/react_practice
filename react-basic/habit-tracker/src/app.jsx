567a511b8d69fbfca2760ba6fd0f72c31f29e32a
HyeJung <gpwjd248@gmail.com>
2022-04-02 18:20:13 +0900

    habit-tracker: JSX

--- react-basic/habit-tracker/src/app.jsx
+++ react-basic/habit-tracker/src/app.jsx
@@ -1,7 +1,21 @@
+import React from 'react';
 import './app.css';
 
 function App() {
-    return <h1>Hello :)</h1>;
+    const name = undefined;
+    // class="" (x) -> className =""
+    // onclick="" (x) -> onClick (대문자)
+    return (
+        // 형제 노드가 있을 때는 묶어줘야 함
+        // <></> 아무것도 작성하지 않아도 React.Fragment를 작성한 것과 동일
+        <React.Fragment>
+            <h1>Hello</h1>
+            {name && <h1>Hello {name} :)</h1>}
+            {['💕', '🍎'].map((item) => (
+                <h1>{item}</h1>
+            ))}
+        </React.Fragment>
+    );
 }
 
 export default App;
