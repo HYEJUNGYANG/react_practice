2b8a46394af99d065d551e272a7d467f169bd2fa
HyeJung <gpwjd248@gmail.com>
2022-04-02 22:29:21 +0900

    habit-tracker: event handling - part1

--- react-basic/habit-tracker/src/components/habit.jsx
+++ react-basic/habit-tracker/src/components/habit.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
 export default class Habit extends Component {
-    state = {
-        count: 0,
-    };
-
     handleIncrement = () => {
-        // state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야 함.
-        this.setState({ count: this.state.count + 1 });
+        this.props.onIncrement(this.props.habit);
     };
 
     handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({ count: count < 0 ? 0 : count });
+        this.props.onDecrement(this.props.habit);
+    };
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
     };
 
     render() {
@@ -33,7 +31,10 @@ export default class Habit extends Component {
                 >
                     <i className="fa-solid fa-square-minus"></i>
                 </button>
-                <button className="habit-button habit-delete">
+                <button
+                    className="habit-button habit-delete"
+                    onClick={this.handleDelete}
+                >
                     <i className="fa-solid fa-trash"></i>
                 </button>
             </li>
