6b8480556e47c9d95f6679a3e08ca488ce2ea8dc
HyeJung <gpwjd248@gmail.com>
2022-04-02 18:59:45 +0900

    habit-tracker: make habit component

--- react-basic/habit-tracker/src/app.jsx
+++ react-basic/habit-tracker/src/app.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
     const name = undefined;
     // class="" (x) -> className =""
     // onclick="" (x) -> onClick (대문자)
-    return (
-        // 형제 노드가 있을 때는 묶어줘야 함
-        // <></> 아무것도 작성하지 않아도 React.Fragment를 작성한 것과 동일
-        <React.Fragment>
-            <h1>Hello</h1>
-            {name && <h1>Hello {name} :)</h1>}
-            {['💕', '🍎'].map((item) => (
-                <h1>{item}</h1>
-            ))}
-        </React.Fragment>
-    );
+    // return (
+    //     // 형제 노드가 있을 때는 묶어줘야 함
+    //     // <></> 아무것도 작성하지 않아도 React.Fragment를 작성한 것과 동일
+    //     <React.Fragment>
+    //         <h1>Hello</h1>
+    //         {name && <h1>Hello {name} :)</h1>}
+    //         {['💕', '🍎'].map((item) => (
+    //             <h1>{item}</h1>
+    //         ))}
+    //     </React.Fragment>
+    // );
+
+    return <Habit />;
 }
 
 export default App;
