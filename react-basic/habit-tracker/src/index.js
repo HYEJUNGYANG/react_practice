9e5c86c5e634887285722c147284e289f325da0b
HyeJung <gpwjd248@gmail.com>
2022-04-02 17:46:10 +0900

    make project

---
+++ react-basic/habit-tracker/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+
+ReactDOM.render(
+    // <React.StrictMode></React.StrictMode> -> 생략 가능
+    //   -> 엄격모드
+    // app.jsx에 정의된 컴포넌트
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root') // index.html에 있는 id='root'인 div에 연결
+);
