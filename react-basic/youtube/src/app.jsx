5b4641e8e43fce857fa4b6d435a1b611d20f2a9d
HyeJung <gpwjd248@gmail.com>
2022-04-05 04:26:36 +0900

    youtube: make search function

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -5,6 +5,23 @@ import VideoList from './components/video_list/video_list';
 
 function App() {
     const [videos, setVideos] = useState([]);
+    const search = (query) => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+        };
+
+        fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCLiVZdH4jKncWMuyb-3P83ghu8NJRR2iU`,
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((result) =>
+                result.items.map((item) => ({ ...item, id: item.id.videoId }))
+            )
+            .then((items) => setVideos(items))
+            .catch((error) => console.log('error', error));
+    };
 
     useEffect(() => {
         const requestOptions = {
@@ -22,7 +39,7 @@ function App() {
     }, []);
     return (
         <div className={styles.app}>
-            <SearchHeader />
+            <SearchHeader onSearch={search} />
             <VideoList videos={videos} />
         </div>
     );
