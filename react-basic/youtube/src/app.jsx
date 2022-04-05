e3fc65f60ecc89fc7d24498fe30bffa66bdf6a04
HyeJung <gpwjd248@gmail.com>
2022-04-05 21:06:12 +0900

    youtube: select video & show video

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 function App({ youtube }) {
     // 컴포넌트 안에서 네트워크 처리를 하는 것은 좋지 않음 💩
     // 키같은 credential은 절대 코드에 남겨두면 안됨 💩
     const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const selectVideo = (video) => {
+        setSelectedVideo(video);
+    };
+
     const search = (query) => {
         youtube
             .search(query) //
@@ -21,7 +28,20 @@ function App({ youtube }) {
     return (
         <div className={styles.app}>
             <SearchHeader onSearch={search} />
-            <VideoList videos={videos} />
+            <section className={styles.content}>
+                {selectedVideo && (
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                )}
+                <div className={styles.list}>
+                    <VideoList
+                        videos={videos}
+                        onVideoClick={selectVideo}
+                        display={selectedVideo ? 'list' : 'grid'}
+                    />
+                </div>
+            </section>
         </div>
     );
 }
