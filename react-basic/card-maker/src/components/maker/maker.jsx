dbf774f36ac81dd74a1fe2de89d1353a054ea7b5
HyeJung <gpwjd248@gmail.com>
2022-04-09 03:55:22 +0900

    card-maker: maker card UI

--- react-basic/card-maker/src/components/maker/maker.jsx
+++ react-basic/card-maker/src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'Jung',
+            company: 'none',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'jung@naver.com',
+            message: 'go for it',
+            fileName: 'jung',
+            fileURL: null,
+        },
+        {
+            id: '2',
+            name: 'Jung',
+            company: 'none',
+            theme: 'dark',
+            title: 'Software Engineer',
+            email: 'jung@naver.com',
+            message: 'go for it',
+            fileName: 'jung',
+            fileURL: null,
+        },
+        {
+            id: '3',
+            name: 'Jung',
+            company: 'none',
+            theme: 'colorful',
+            title: 'Software Engineer',
+            email: 'jung@naver.com',
+            message: 'go for it',
+            fileName: 'jung',
+            fileURL: null,
+        },
+    ]);
     const history = useHistory();
     const onLogout = () => {
         if (window.confirm('로그아웃 하시겠습니까?')) {
@@ -25,8 +60,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer />
         </section>
